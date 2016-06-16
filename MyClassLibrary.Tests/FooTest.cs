ead6ac71a1c58806b3ab4a942c57631cca6391b9
nicopaez <nicopaez@computer.org>
2016-06-16 11:34:26 -0700

    Initial commit

---
+++ MyClassLibrary.Tests/FooTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary.Tests
+{
+    [TestFixture]
+    public class FooTest
+    {
+        [Test]
+        public void DoFooShouldReturnFoo()
+        {
+            Foo foo = new Foo();
+            Assert.AreEqual("Foo", foo.DoFoo());
+        }
+    }
+}
